245f2b8ea91c430dd37789d6bb1868b01379fc6b
Jiashen Cao <jcao62@lawn-143-215-63-178.lawn.gatech.edu>
2023-03-01 15:03:57 -0500

    update

--- 3dbar_plot.py
+++ 3dbar_plot.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-
-
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+
+from collections import defaultdict
 
 from config import *
 
@@ -16,12 +16,6 @@ def main():
         type=str,
         help="Data file path.",
     )
-    parser.add_argument(
-        "--stack",
-        action="store_true",
-        default=False,
-        help="Whether to draw stacked bar plots.",
-    )
     parser.add_argument("--fig-size", nargs="+", type=int, default=(5, 2))
     args = parser.parse_args()
 
@@ -33,50 +27,51 @@ def main():
         COLOR["green"],
     ]
 
-    sub_cat_length, sub_cat, sub_cat_data = 0, [], []
-    cat = []
+    sub_cat_repeat, sub_cat_length, sub_cat, sub_cat_data_group = 0, 0, [], []
+    cat = set([])
+
+    repeat_idx = 0
 
     # collect data
     with open(args.data_path) as f:
         for i, line in enumerate(f.read().splitlines()):
             if i == 0:
                 sub_cat = line.split(",")
-                sub_cat_length = len(sub_cat)
-                sub_cat_data = [[] for _ in range(sub_cat_length)]
+                sub_cat_length = len(sub_cat) - 1
+                sub_cat_repeat = int(sub_cat[-1])
+                sub_cat_data_group = [
+                    [[] for _ in range(sub_cat_length)] for _ in range(sub_cat_repeat)
+                ]
             else:
                 for k, d in enumerate(line.split(",")):
                     if k < sub_cat_length:
-                        sub_cat_data[k].append(float(d))
+                        sub_cat_data_group[repeat_idx][k].append(float(d))
                     else:
-                        cat.append(d)
+                        cat.add(d)
+                repeat_idx = (repeat_idx + 1) % sub_cat_repeat
 
     # figure creation
     fig, ax = plt.subplots(figsize=args.fig_size)
 
-    # draw bar chart
-    if args.stack:
+    for i in range(sub_cat_length):
         bottom = np.zeros(len(cat))
-        for i in range(sub_cat_length):
-            x = np.arange(len(cat)) * (BAR_WIDTH + BAR_GAP)
-            height = sub_cat_data[i]
-            ax.bar(x, height, color=color[i], bottom=bottom, label=sub_cat[i])
-            bottom += height
-        xtick_list = np.arange(len(cat)) * (BAR_WIDTH + BAR_GAP)
-        xtick_label_list = cat
-    else:
-        for i in range(sub_cat_length):
+        for k in range(sub_cat_repeat):
             x = (
                 np.arange(len(cat)) * (sub_cat_length * BAR_WIDTH + BAR_GAP)
                 + i * BAR_WIDTH
             )
-            height = sub_cat_data[i]
-            ax.bar(x, height, color=color[i], label=sub_cat[i])
-        xtick_list = (
-            np.arange(len(cat)) * (sub_cat_length * BAR_WIDTH + BAR_GAP)
-            + sub_cat_length * BAR_WIDTH / 2
-            - BAR_WIDTH / 2
-        )
-        xtick_label_list = cat
+            height = sub_cat_data_group[k][i]
+            if k == 0:
+                ax.bar(x, height, color=color[i], bottom=bottom, label=sub_cat[i])
+            else:
+                ax.bar(x, height, color=color[i], bottom=bottom)
+            bottom += np.array(height)
+    xtick_list = (
+        np.arange(len(cat)) * (sub_cat_length * BAR_WIDTH + BAR_GAP)
+        + sub_cat_length * BAR_WIDTH / 2
+        - BAR_WIDTH / 2
+    )
+    xtick_label_list = cat
 
     ax.set_xticks(xtick_list)
     ax.set_xticklabels(xtick_label_list)
@@ -90,7 +85,7 @@ def main():
     )
 
     plt.tight_layout()
-    plt.savefig("bar.pdf")
+    plt.savefig("3dbar.pdf")
 
 
 if __name__ == "__main__":
